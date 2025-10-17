a2a3433e4baa8394d37928564ee55fcae57fa460
mwasobaddy <kelvinramsiel@gmail.com>
2025-10-17 12:59:57 +0300

    feat: add font loading and loader to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import * as Font from "expo-font";
+import { Stack } from "expo-router";
+import React, { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+import "./globals.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    Font.loadAsync({
+      "Roboto-Regular": require("../assets/fonts/Roboto/static/Roboto-Regular.ttf"),
+      "Roboto-Bold": require("../assets/fonts/Roboto/static/Roboto-Bold.ttf"),
+      "Roboto-Medium": require("../assets/fonts/Roboto/static/Roboto-Medium.ttf"),
+      "Roboto-Light": require("../assets/fonts/Roboto/static/Roboto-Light.ttf")
+    }).then(() => setFontsLoaded(true));
+  }, []);
+
+  if (!fontsLoaded) {
+    return <Loader />;
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+    </Stack>
   );
 }
